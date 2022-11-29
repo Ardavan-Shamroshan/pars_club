50dac0967cba8a1925ab3dfcb869081227d4b269
Ardavan-Shamroshan <ardavan2000.ashr@gmail.com>
2022-11-29 18:57:23 +0330

    layouts completed - public/modules folder created - admin panel connected

---
+++ Modules/Admin/Resources/views/layouts/scripts.blade.php
@@ -0,0 +1,27 @@
+<!-- Back-to-top -->
+<a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a><!-- Jquery js-->
+<script src="{{ asset('modules/admin/assets/plugins/jquery/jquery.min.js') }}"></script><!-- Bootstrap js-->
+<script src="{{ asset('modules/admin/assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/plugins/bootstrap/js/bootstrap-rtl.js') }}"></script><!-- Ionicons js-->
+<script src="{{ asset('modules/admin/assets/plugins/ionicons/ionicons.js') }}"></script><!-- Moment js -->
+<script src="{{ asset('modules/admin/assets/plugins/moment/moment.js') }}"></script><!-- P-scroll js -->
+<script src="{{ asset('modules/admin/assets/plugins/perfect-scrollbar/perfect-scrollbar.min-rtl.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/plugins/perfect-scrollbar/p-scroll-rtl.js') }}"></script><!-- Rating js-->
+<script src="{{ asset('modules/admin/assets/plugins/rating/jquery.rating-stars.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/plugins/rating/jquery.barrating.js') }}"></script><!-- Sticky js -->
+<script src="{{ asset('modules/admin/assets/js/sticky.js') }}"></script><!-- sidemenu js -->
+<script id="sidemenu" src="{{ asset('modules/admin/assets/plugins/side-menu/sidemenu.js') }}"></script><!-- Right-sidebar js -->
+<script src="{{ asset('modules/admin/assets/plugins/sidebar/sidebar-rtl.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/plugins/sidebar/sidebar-custom.js') }}"></script><!-- eva-icons js -->
+<script src="{{ asset('modules/admin/assets/plugins/eva-icons/eva-icons.min.js') }}"></script><!-- Moment js -->
+<script src="{{ asset('modules/admin/assets/plugins/raphael/raphael.min.js') }}"></script><!--Internal  Chart.bundle js -->
+<script src="{{ asset('modules/admin/assets/plugins/chartjs/Chart.bundle.min.html') }}"></script><!--Internal Sparkline js -->
+<script src="{{ asset('modules/admin/assets/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script><!--Internal Apexchart js-->
+<script src="{{ asset('modules/admin/assets/js/apexcharts.js') }}"></script><!-- Internal Map -->
+<script src="{{ asset('modules/admin/assets/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script><!--Internal  index js -->
+<script src="{{ asset('modules/admin/assets/js/index.js') }}"></script><!-- Apexchart js-->
+<script src="{{ asset('modules/admin/assets/js/apexcharts.js') }}"></script>
+<script src="{{ asset('modules/admin/assets/js/jquery.vmap.sampledata.js') }}"></script><!-- custom js -->
+<script src="{{ asset('modules/admin/assets/js/custom.js') }}"></script><!-- Switcher js -->
+<script src="{{ asset('modules/admin/assets/switcher/js/switcher-rtl.js') }}"></script>
